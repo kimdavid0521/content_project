c012f196a3e7b09ee9355498345de3850171b81f
kimtaeyoung <trust00005@naver.com>
2023-11-09 15:37:31 +0900

    스플래시 화면 구현

---
+++ game-project/js/splash.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      const splashScreen = document.querySelector(".splash-screen");
+      splashScreen.style.display = "none";
+      window.location.href="/dksds";
+    }, 3000); 
+  });
+  
