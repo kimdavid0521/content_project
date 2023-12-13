37b5560a6109a96e8e1fc465d9d5408447066c42
kimtaeyoung <trust00005@naver.com>
2023-12-14 01:04:03 +0900

    엔딩부분 사용자 이름 조회 로직 구현

--- game-project/js/ending_page.js
+++ game-project/js/ending_page.js
@@ -1,3 +1,6 @@
 function movepage () {
     window.location.href="../page/splash_page.html";
 }
+
+const username = localStorage.getItem("name");
+document.getElementById("username").innerHTML =  username;
